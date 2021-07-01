a093d103d394a34e665e0945f723abc9aa3cb543
madison nguyen <madison.nguyen565@student.mckinneyisd.net>
2021-07-01 10:15:31 -0500

    Attempt to push for ToDo

---
+++ TranquiliTree/CompleteToDoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CompleteToDoViewController.swift
+//  TranquiliTree
+//
+//  Created by Madison Nguyen on 7/1/21.
+//  Copyright © 2021 Kennedi Robison. All rights reserved.
+//
+
+import UIKit
+
+class CompleteToDoViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func completeTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
