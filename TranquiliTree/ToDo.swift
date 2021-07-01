c5e34ce2f99ec6aecc8eab2ea35d595693f7f0de
madison nguyen <madison.nguyen565@student.mckinneyisd.net>
2021-07-01 10:11:04 -0500

    Attempt at ToDo

---
+++ TranquiliTree/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  TranquiliTree
+//
+//  Created by Madison Nguyen on 7/1/21.
+//  Copyright © 2021 Kennedi Robison. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
