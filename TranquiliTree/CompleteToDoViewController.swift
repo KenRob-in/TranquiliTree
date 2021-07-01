1e54e30a129527b0965387d3d7a601fe3f6ba566
madison nguyen <madison.nguyen565@student.mckinneyisd.net>
2021-07-01 11:24:53 -0500

    Font  Chosen

--- TranquiliTree/CompleteToDoViewController.swift
+++ TranquiliTree/CompleteToDoViewController.swift
@@ -4,20 +4,25 @@
 //
 //  Created by Madison Nguyen on 7/1/21.
 //  Copyright © 2021 Kennedi Robison. All rights reserved.
-//
+
 
 import UIKit
 
 class CompleteToDoViewController: UIViewController {
-
+    
+    var previousVC = ToDoTableTableViewController()
+    var selectedToDo = ToDo()
     @IBOutlet weak var titleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        titleLabel.text = selectedToDo.name
     }
     
     @IBAction func completeTapped(_ sender: Any) {
+    
+            
     }
     
     /*
@@ -28,6 +33,7 @@ class CompleteToDoViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
+    
+*/
 }
+
